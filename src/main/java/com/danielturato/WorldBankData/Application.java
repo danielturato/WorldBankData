52718b3d54bac2a6d4a1d5730728776c8e3ac9d6
Daniel <dturato@hotmail.com>
2018-12-13 13:12:40 +0000

    Initial commit

---
+++ src/main/java/com/danielturato/WorldBankData/Application.java
@@ -0,0 +1,97 @@
+package com.danielturato.WorldBankData;
+
+
+import com.danielturato.WorldBankData.Model.Country;
+import com.danielturato.WorldBankData.Model.Country.CountryBuilder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import java.util.Scanner;
+
+public class Application {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int option = 0;
+        do {
+            displayMenu();
+            System.out.printf("%n%nEnter your choice: ");
+            while (!input.hasNextInt()) {
+                System.out.println("That's not a valid input! Try again");
+                input.next();
+            }
+            option = input.nextInt();
+            validateInput(option);
+        } while (option != 6);
+        System.exit(0);
+    }
+
+    private static void displayMenu() {
+        System.out.printf("%n******WORLD BANK DATA******%n%n");
+        System.out.println("1)     View data table");
+        System.out.println("2)     View statistics");
+        System.out.println("3)     Add a country");
+        System.out.println("4)     Edit a country");
+        System.out.println("5)     Delete a country");
+        System.out.println("6)     Quit");
+    }
+
+    private static void validateInput(int option) {
+        switch (option) {
+            case 1:
+                displayCountries();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                System.out.printf("%nThat's not a valid menu choice! Try again%n");
+                break;
+        }
+    }
+
+    private static void displayCountries() {
+        List<Country> countries = fetchAllCountries();
+        System.out.println("Country                            Internet Users           Literacy");
+        System.out.println("--------------------------------------------------------------------");
+        countries.stream().forEach(System.out::println);
+    }
+
+    private static List<Country> fetchAllCountries() {
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<Country> criteria = builder.createQuery(Country.class);
+
+        criteria.from(Country.class);
+
+        List<Country> countries = session.createQuery(criteria).getResultList();
+
+        session.close();
+
+        return countries;
+    }
+
+}
---
+++ src/main/java/com/danielturato/WorldBankData/Model/Country.java
@@ -0,0 +1,96 @@
+package com.danielturato.WorldBankData.Model;
+
+import javax.persistence.*;
+import java.text.DecimalFormat;
+
+@Entity
+public class Country {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String code;
+
+    @Column(length = 32)
+    private String name;
+
+    @Column(length = 11)
+    private Double internetUsers;
+
+    @Column(length = 11)
+    private Double adultLiteracyRate;
+
+    public Country() {}
+
+    public Country(CountryBuilder countryBuilder) {
+        this.name = countryBuilder.name;
+        this.internetUsers = countryBuilder.internetUsers;
+        this.adultLiteracyRate = countryBuilder.adultLiteracyRate;
+    }
+
+    @Override
+    public String toString() {
+        String name = getName() != null? getName() : "--";
+        String internetUsers = getInternetUsers() != null? new DecimalFormat("#0.##").format(getInternetUsers()) : "--";
+        String adultLiteracyRate = getAdultLiteracyRate() != null? new DecimalFormat("#0.##").format(getAdultLiteracyRate()) : "--";
+
+        return String.format("%-35s%-25s%s", name, internetUsers, adultLiteracyRate);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getInternetUsers() {
+        return internetUsers;
+    }
+
+    public void setInternetUsers(Double internetUsers) {
+        this.internetUsers = internetUsers;
+    }
+
+    public Double getAdultLiteracyRate() {
+        return adultLiteracyRate;
+    }
+
+    public void setAdultLiteracyRate(Double adultLiteracyRate) {
+        this.adultLiteracyRate = adultLiteracyRate;
+    }
+
+    public static class CountryBuilder {
+        private String name;
+        private Double internetUsers;
+        private Double adultLiteracyRate;
+
+        public CountryBuilder(String name) {
+            this.name = name;
+        }
+
+        public CountryBuilder withInternetUsers(Double internetUsers) {
+            this.internetUsers = internetUsers;
+            return this;
+        }
+
+        public CountryBuilder withAdultLiteracyRate(Double literacyRate) {
+            this.adultLiteracyRate = literacyRate;
+            return this;
+        }
+
+        public Country build() {
+            return new Country(this);
+        }
+
+
+    }
+}
